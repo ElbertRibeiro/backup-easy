74f18076aec1a742e0b268284d97d03c473b3f51
Elbert Nilton Batista Ribeiro <elbertprofissional@gmail.com>
2020-03-03 19:56:38 +0000

    update code

--- backup.py
+++ backup.py
@@ -1,20 +1,26 @@
 #Constroi o arquivo e path de backup e retorna
+from datetime import datetime, date
 
-from datetime import datetime
+class DataHora:
+    now = datetime.now()
+    print ( now.hour, ':', now.minute, '  ', now.day, '-', 
+        now.month, '-', now.year )
 
-def gerabackup():
-    date = ( time.strftime("%Y-%m-%d") )
-    
+
+class backup:
+    def gerabackup():
+        date = (time.strftime("%Y-%m-%d"))
+
     #cria o nome do arquivo de backup
-    backupfile = '%s-backup-full.tar.gz' % date 
+        backupfile = '%s-backup-full.tar.gz' % date
 
     #destino onde será gravado o backup
-    pathdesdino = '/mnt/hdbackup/%s' % backupfile
+        pathdesdino = '/mnt/hdbackup/%s' % backupfile
 
     #pasta que será feita backup
-    pathorigem = '/mnt/storage'
+        pathorigem = '/mnt/storage'
 
     #comando de execução
-    backup = 'tar cvf %s %s' % (pathdesdino, pathorigem)
+        backup = 'tar cvf %s %s' % (pathdesdino, pathorigem)
 
-    return backup
+        return backup
