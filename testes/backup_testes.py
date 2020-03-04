68f6e79aaa8e76d5f11a5b712ddde846d55faf48
Elbert Nilton Batista Ribeiro <elbertprofissional@gmail.com>
2020-03-04 20:56:49 +0000

    update

--- testes/backup_testes.py
+++ testes/backup_testes.py
@@ -1,8 +1,38 @@
 import unittest
 
-def fun(x):
-    return ( x + 1 )
 
-class MyTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(fun(3), 4)
+def soma(a, b):
+    u"""
+        Função faz a soma de dois números.
+
+        Ex:
+            >> soma(1, 2)
+            3
+            >> soma(3, 4)
+            7
+    """
+    return a + b
+
+
+# Precisamos herdar do unittest.TestCase
+# para que nossos test cases sejam reconhecidos
+class TestSoma(unittest.TestCase):
+
+    # cada test case é uma função separada e independente
+    def test_soma_numeros_positivos(self):
+        resultado = soma(1, 2)
+
+        # usamos um metodo padrão do TestCase para verificar se o
+        # valor é igual
+        # poderiamos ter feito de uma forma mais simples utilizando
+        # assert resultado == 3
+        self.assertEqual(resultado, 3)
+
+    def test_soma_numeros_negativos(self):
+        resultado = soma(1, -1)
+        self.assertEqual(resultado, 0)
+
+
+if __name__ == '__main__':
+    # Executa todos os testes desse arquivo
+    unittest.main()
